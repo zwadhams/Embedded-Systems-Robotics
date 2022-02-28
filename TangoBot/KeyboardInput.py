97dd9f35ecb945b274f3414dde5ba6188fd9b010
Kruize Derek Christensen <christensen.kruize@gmail.com>
2022-02-28 12:15:35 -0700

    Started Keyboard Commands
    Started just need values the key set it to also am considering making number keys the speed

--- TangoBot/KeyboardInput.py
+++ TangoBot/KeyboardInput.py
@@ -1,5 +1,6 @@
 import time, serial, sys
 import tkinter as tk
+import keyboard as ky
 
 class Keyboard_Input:
     DEFAULT_VALID = {'<Up>', '<Left>', '<Down>', '<Right>', '<space>', '<z>', '<c>', '<w>', '<s>', '<a>', '<d>'}
@@ -8,3 +9,22 @@ class Keyboard_Input:
         self.control_list = control_list
         return
     pass
+
+while True:
+    while ky.is_pressed("w") or ky.is_pressed("W"):
+        #rightMotor
+        #leftMotor
+        print("Forward")
+    
+    while ky.is_pressed("w") or ky.is_pressed("W"):
+        #rightMotor
+        #leftMotor
+        print("Forward")
+        
+    while ky.is_pressed("r") or ky.is_pressed("R"):
+        #rightMotor negative value
+        #leftMotor positive value
+        print("Right")
+        
+    #rightMotor 6000 or off
+    #leftMotor 6000 or off
