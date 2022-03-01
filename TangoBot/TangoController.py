e52eb249ba7618566dada7a9252028cd3c9e16d9
ronnelwalton <59928379+ronnelwalton@users.noreply.github.com>
2022-02-28 21:31:36 -0700

    Update TangoController.py
    - Updated to Simultaneous control to both servos and motors
         - Now stores the sent values to the servo/motor driver in a byte array
         - Will only update the servo/motors with send_command() function

--- TangoBot/TangoController.py
+++ TangoBot/TangoController.py
@@ -1,14 +1,24 @@
+import time
 class Tango_Controller:
-    RESOLUTION = 4000
+    DEBUG = True
+    RESOLUTION = 2000
     MID_POSITION = 6000
-    SERVO_STEP = 5
-    SERVO_STEP_MAX = MID_POSITION + ((RESOLUTION/2))
-    SERVO_STEP_MIN = MID_POSITION - ((RESOLUTION/2))
-    MODE = "S" # S for Single Target, M for Multiple Target
+    MOTOR_STEPS = 3
+    WAIST_STEPS = 3
+    HEAD_STEPS = 5
+    MOTOR_STEP = int(RESOLUTION/MOTOR_STEPS)
+    WAIST_SERVO_STEP = int(RESOLUTION/WAIST_STEPS)
+    HEAD_SERVO_STEP = int(RESOLUTION/HEAD_STEPS)
+    NUMBER_OF_CHANNELS = 0
+    CHANNEL_START = 0
+    SERVO_STEP_MAX = MID_POSITION + RESOLUTION
+    SERVO_STEP_MIN = MID_POSITION - RESOLUTION
     DEFAULT_VALUES = {"Waist": MID_POSITION, "Neck_Pan": MID_POSITION, "Neck_Tilt": MID_POSITION, "Right_Wheel": 0, "Left_Wheel": 0}
-    serialQueue = []
+    # | 0x9F                 | 5                  | 0                 | 0x##     | 0x##   | 0x##     | 0x##   | 0x##     | 0x##   |....
+    # | simultaneous command | Number of Channels | Channel Number    | Ch0LSB   | CH0MSB | Ch1LSB   | CH1MSB | Ch2LSB   | CH2MSB |....
+    serialString = [0x9F, NUMBER_OF_CHANNELS, CHANNEL_START] # This contains all servo and motor values. DO NOT CLEAR. Only change the value
     class Servo_Motor:
-        def __init__(self, id, type, channel, speed = 0, target = 0):
+        def __init__(self, id:str, type:str, channel:int, speed:int = 0, target:int = 0):
             self.id = id
             self.channel = channel
             self.type = type
@@ -16,7 +26,7 @@ class Tango_Controller:
             self.target = target
             return
         
-        def set_channel(self, channel):
+        def set_channel(self, channel:int):
             self.channel = channel
             return
         
@@ -26,139 +36,192 @@ class Tango_Controller:
         def get_type(self):
             return self.type
         
-        def set_speed(self, speed):
+        def set_speed(self, speed:int):
             self.speed = speed
+            return
+        
+        def adjust_speed(self, speed:int):
+            self.speed += speed
             return
         
         def get_speed(self):
             return self.speed
         
-        def set_target(self, target):
+        def set_target(self, target:int):
             self.target = target
             
         def get_target(self):
             return self.target
         
         def encode_target(self):
-            return [chr(self.target&0x7F), chr((self.target>>7)&0x7F)]
-                    
+            return [self.target&0x7F, (self.target>>7)&0x7F]
+
         def encode_speed(self):
-            return [chr((Tango_Controller.MID_POSITION - self.speed)&0x7F), chr(((Tango_Controller.MID_POSITION - self.speed)>>7)&0x7F)]
+            target = (Tango_Controller.MID_POSITION-self.speed)
+            return [target & 0x7F, (target>>7) & 0x7F]
         pass
-    DEBUG = True
-    def __init__(self, serialController, mode):
+    def __init__(self, serialController):
         self.serial = serialController
-        self.mode = mode
         self.motors = {"Right_Wheel": self.Servo_Motor("Right_Wheel", "Motor", 1), "Left_Wheel": self.Servo_Motor("Left_Wheel", "Motor", 2)}
         self.servos = {"Waist": self.Servo_Motor("Waist", "Servo", 0), "Neck_Pan": self.Servo_Motor("Neck_Pan", "Servo", 3), "Neck_Tilt": self.Servo_Motor("Neck_Tilt", "Servo", 4)}
-        
+        self.NUMBER_OF_CHANNELS = len(self.motors.keys()) + len(self.servos.keys())
+        self.serialString[1] = self.NUMBER_OF_CHANNELS
         self.init_motors()
         self.init_servos()
+        self.send_command()
         return
     
+    # Checks
+    def is_valid_target(self, target):
+        return (target >= self.SERVO_STEP_MIN) & (target <= self.SERVO_STEP_MAX)
+
     # Initializing
     def init_motors(self):
         print("Initializing Motors") if self.DEBUG else None
         motorList = self.motors.keys()
         for motor in motorList:
-            self.motors[motor].set_speed(self.DEFAULT_VALUES[motor])
+            motorObj = self.motors[motor]
+            motorObj.set_speed(self.DEFAULT_VALUES[motor])
+            self.update_serial_string(motorObj)
         return
     
     def init_servos(self):
         print("Initializing Servos") if self.DEBUG else None
         servoList = self.servos.keys()
         for servo in servoList:
-            self.servos[servo].set_target(self.DEFAULT_VALUES[servo])
+            servoObj = self.servos[servo]
+            servoObj.set_target(self.DEFAULT_VALUES[servo])
+            self.update_serial_string(servoObj)
         return
     
     # Robot Serial Controls
-    def add_serial_queue(self, command):
-        print("Command Added ", command) if self.DEBUG else None
-        self.serialQueue.append(command)
+    def update_serial_string(self, servoMotor:Servo_Motor):
+        targetSpeed = []
+        if servoMotor.get_type() == "Servo":
+            targetSpeed = servoMotor.encode_target()
+        elif servoMotor.get_type() == "Motor":
+            targetSpeed = servoMotor.encode_speed()
+        else:
+            return
+        print("Serial String Updated") if self.DEBUG else None
+        self.serialString[3 + (2*(servoMotor.get_channel()))] = targetSpeed[0]
+        self.serialString[4 + (2*(servoMotor.get_channel()))] = targetSpeed[1]
         return
 
     def send_command(self):
-        print("Command Sent ", self.serialQueue) if self.DEBUG else None
-        self.serial.write(self.serialQueue.encode())
-        self.serialQueue = []
+        print("Command Sent {}".format(bytearray(self.serialString))) if self.DEBUG else None
+        self.serial.write(bytearray(self.serialString))
         return
     
-    def control_motor(self, motorId, speed):
+    # Movement Set (Works in tandem with Robot Serial Controls)  
+    def control_motor(self,motorId:str, speed:int):
         print("Controlling Motor ", motorId) if self.DEBUG else None
         motor = self.motors[motorId]
         motor_speed = self.MID_POSITION - speed
-        if (motor_speed > self.SERVO_STEP_MIN) & (motor_speed < self.SERVO_STEP_MAX):
+        if self.is_valid_target(motor_speed):
             motor.set_speed(speed)
         else:
             return
-        start_byte = chr(0x84)
-        self.add_serial_queue(start_byte)
-        self.add_serial_queue(motor.get_channel())
-        self.add_serial_queue(motor.encode_speed()[0])
-        self.add_serial_queue(motor.encode_speed()[1])
+        self.update_serial_string(motor)
         self.send_command()
         return
 
-    def adjust_motor(self, motorId, speed):
+    def control_wheels(self, RSpeed:int, LSpeed:int):
+        print("Controlling Wheels") if self.DEBUG else None
+        Rmotor = self.motors["Right_Wheel"]
+        Lmotor = self.motors["Left_Wheel"]
+        RMotorSpeed = self.MID_POSITION - RSpeed
+        LMotorSpeed = self.MID_POSITION - LSpeed
+        if self.is_valid_target(RMotorSpeed) & self.is_valid_target(LMotorSpeed):
+            Rmotor.set_speed(RSpeed)
+            Lmotor.set_speed(LSpeed)
+        else:
+            return
+        self.update_serial_string(Rmotor)
+        self.update_serial_string(Lmotor)
+        self.send_command()
+        return
+
+    def adjust_motor(self, motorId:str, speed:int):
         self.control_motor(motorId, self.motors[motorId].get_speed() + speed)
         return
 
-    def control_servo(self, servoId, position):
+    def control_servo(self, servoId:str, position:int):
         print("Controlling Servo ", servoId) if self.DEBUG else None
         servo = self.motors[servoId]
         target = position
-        if (target > self.SERVO_STEP_MIN) & (target < self.SERVO_STEP_MAX):
+        if self.is_valid_target(target):
             servo.set_target(target)
         else:
             return
-        start_byte = chr(0x84)
-        self.add_serial_queue(start_byte)
-        self.add_serial_queue(servo.get_channel())
-        self.add_serial_queue(servo.encode_target()[0])
-        self.add_serial_queue(servo.encode_target()[1])
+        self.update_serial_string(servo)
         self.send_command()
         return
 
-    def adjust_motor(self, servoId, target):
+    def adjust_servo(self, servoId:str, target:int):
         self.control_servo(servoId, self.servos[servoId].get_target() + target)
         return
     
-    # Movement Set (Works in tandem with Robot Serial Controls)        
+    # Movement Wrappers
     def stop(self):
         print("Wheels Stopping") if self.DEBUG else None
-        while (self.MID_POSITION != self.motors["Left_Wheel"].get_speed()) | (self.MID_POSITION != self.motors["Right_Wheel"].get_speed()):
-            if self.motors["Left_Wheel"].get_speed() < 0:
-                self.adjust_motor("Left_Wheel", self.SERVO_STEP)
+        while (0 != self.motors["Right_Wheel"].get_speed()) | (0 != self.motors["Left_Wheel"].get_speed()):
+            print("RSpeed: ", self.motors["Right_Wheel"].get_speed())
+            print("LSpeed: ", self.motors["Left_Wheel"].get_speed())
+            RMotor = self.motors["Right_Wheel"]
+            LMotor = self.motors["Left_Wheel"]
+            if (RMotor.get_speed() == 0):
+                None
+            elif RMotor.get_speed() < 0:
+                RMotor.adjust_speed(self.MOTOR_STEP)
             else:
-                self.adjust_motor("Right_Wheel", -self.SERVO_STEP)
-            if self.motors["Right_Wheel"].get_speed() < 0:
-                self.adjust_motor("Right_Wheel", self.SERVO_STEP)
+                RMotor.adjust_speed(-self.MOTOR_STEP)
+            if (LMotor.get_speed() == 0):
+                None
+            elif LMotor.get_speed() < 0:
+                LMotor.adjust_speed(self.MOTOR_STEP)
             else:
-                self.adjust_motor("Right_Wheel", -self.SERVO_STEP)
+                LMotor.adjust_speed(-self.MOTOR_STEP)
+            self.update_serial_string(RMotor)
+            self.update_serial_string(LMotor)
+            self.send_command()
+            time.sleep(0.2)
         return
     
-    def forward(self, speed):
-        print("Wheels Moving Forward") if self.DEBUG else None
+    def forward(self, speed:int):
         if (speed > 0):
-            self.control_motor("Right_Wheel", speed)
-            self.control_motor("Left_Motor", speed)
+            print("Wheels Moving Forward") if self.DEBUG else None
+            self.control_wheels(speed, speed)
         return
     
-    def backward(self, speed):
-        print("Wheels Moving Backward") if self.DEBUG else None
+    def backward(self, speed:int):
         if (speed > 0):
-            self.control_motor("Right_Wheel", -speed)
-            self.control_motor("Left_Motor", -speed)
+            print("Wheels Moving Backward") if self.DEBUG else None
+            self.control_wheels(-speed, -speed)
         return
     
-    def steer_right(self):
-        print("Spinning Right") if self.DEBUG else None
+    def steer_right(self, speed:int):
+        if (speed > 0):
+            print("Spinning Right") if self.DEBUG else None
+        return
+    
+    def steer_left(self, speed:int):
+        if (speed > 0):
+            print("Spinning Left") if self.DEBUG else None
+        return
+    
+    def pan_waist(self, target:int):
+        if (self.is_valid_target(target)):
+            print("Panning Waist") if self.DEBUG else None
+            self.control_servo("Waist", target)
 
-        return
+    def pan_neck(self, target:int):
+        if (self.is_valid_target(target)):
+            print("Panning Neck") if self.DEBUG else None
+            self.control_servo("Neck_Pan", target)
     
-    def steer_left(self):
-        print("Spinning Left") if self.DEBUG else None
-
-        return
-    
+    def tilt_neck(self, target:int):
+        if (self.is_valid_target(target)):
+            print("Tilting Neck") if self.DEBUG else None
+            self.control_servo("Neck_Tilt", target)
     pass
