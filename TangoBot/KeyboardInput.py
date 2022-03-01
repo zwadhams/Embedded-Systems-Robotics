53b2a48744beb9aa8ce6db09d2be2fb3be16cace
Kruize Derek Christensen <christensen.kruize@gmail.com>
2022-03-01 16:16:20 -0700

    Add files via upload
    got it so it reads keyboard inputs
    will make the rest today

--- TangoBot/KeyboardInput.py
+++ TangoBot/KeyboardInput.py
@@ -1,11 +1,26 @@
-import time, serial, sys
-import tkinter as tk
-import keyboard as ky
+#import time, serial, sys
+from tkinter import *
 
-class Keyboard_Input:
-    DEFAULT_VALID = {'<Up>', '<Left>', '<Down>', '<Right>', '<space>', '<z>', '<c>', '<w>', '<s>', '<a>', '<d>'}
-    def __init__(self, win, control_list = DEFAULT_VALID):
-        self.win = win
-        self.control_list = control_list
-        return
-    pass
+root = Tk()
+root.title('Robot')
+root.geometry("400x400")
+
+def clicker(e):
+    myLabel = Label(root, text="Forward")
+    myLabel.pack()
+
+
+root.bind('<w>', clicker)
+
+
+root.mainloop()
+
+
+#------------------
+#class Keyboard_Input:
+#    DEFAULT_VALID = {'<Up>', '<Left>', '<Down>', '<Right>', '<space>', '<z>', '<c>', '<w>', '<s>', '<a>', '<d>'}
+#    def __init__(self, win, control_list = DEFAULT_VALID):
+#        self.win = win
+#        self.control_list = control_list
+#        return
+#    pass
