fa9f54c88b2b87c06d687e10ce2fea9bcb7ca7f5
Kruize Derek Christensen <christensen.kruize@gmail.com>
2022-03-01 18:14:55 -0700

    Add files via upload
    forgot to add a stop when moving

--- TangoBot/KeyboardInput.py
+++ TangoBot/KeyboardInput.py
@@ -1,78 +1,85 @@
 #import time, serial, sys
 from tkinter import *
 
-root = Tk()
-root.title('Robot')
-root.geometry("400x400")
+self = Tk()
 
-def forward(e):
-    myLabel = Label(root, text="Forward")
-    myLabel.pack()
+class Keyboard_Input:
+    self.title('Robot')
+    self.geometry("400x400")
 
-def backward(e):
-    myLabel = Label(root, text="Backward")
-    myLabel.pack()
+    def forward(e):
+        myLabel = Label(self, text="Forward")
+        myLabel.pack()
 
-def turnRight(e):
-    myLabel = Label(root, text="Turning Right")
-    myLabel.pack()
+    def backward(e):
+        myLabel = Label(self, text="Backward")
+        myLabel.pack()
 
-def turnLeft(e):
-    myLabel = Label(root, text="Turning Left")
-    myLabel.pack()
+    def turnRight(e):
+        myLabel = Label(self, text="Turning Right")
+        myLabel.pack()
 
-def twistRight(e):
-    myLabel = Label(root, text="Twisting Right")
-    myLabel.pack()
+    def turnLeft(e):
+        myLabel = Label(self, text="Turning Left")
+        myLabel.pack()
 
-def twistLeft(e):
-    myLabel = Label(root, text="Twisting Left")
-    myLabel.pack()
+    def twistRight(e):
+        myLabel = Label(self, text="Twisting Right")
+        myLabel.pack()
 
-def lookUp(e):
-    myLabel = Label(root, text="Looking Up")
-    myLabel.pack()
+    def twistLeft(e):
+        myLabel = Label(self, text="Twisting Left")
+        myLabel.pack()
 
-def lookDown(e):
-    myLabel = Label(root, text="Looking Down")
-    myLabel.pack()
+    def lookUp(e):
+        myLabel = Label(self, text="Looking Up")
+        myLabel.pack()
 
-def lookRight(e):
-    myLabel = Label(root, text="Looking Right")
-    myLabel.pack()
+    def lookDown(e):
+        myLabel = Label(self, text="Looking Down")
+        myLabel.pack()
 
-def lookLeft(e):
-    myLabel = Label(root, text="Looking Left")
-    myLabel.pack()
+    def lookRight(e):
+        myLabel = Label(self, text="Looking Right")
+        myLabel.pack()
 
-def speed(e):
-    myLabel = Label(root, text="Speed 3")
-    myLabel.pack()
+    def lookLeft(e):
+        myLabel = Label(self, text="Looking Left")
+        myLabel.pack()
 
-def speedMed(e):
-    myLabel = Label(root, text="Speed 2")
-    myLabel.pack()
+    def speedHigh(e):
+        myLabel = Label(self, text="Speed 3")
+        myLabel.pack()
 
-def speedLow(e):
-    myLabel = Label(root, text="Speed 1")
-    myLabel.pack()
+    def speedMed(e):
+        myLabel = Label(self, text="Speed 2")
+        myLabel.pack()
 
-root.bind('<w>', forward)
-root.bind('<s>', backward)
-root.bind('<d>', turnRight)
-root.bind('<a>', turnLeft)
-root.bind('<e>', twistRight)
-root.bind('<q>', twistLeft)
-root.bind('<Up>', lookUp)
-root.bind('<Down>', lookDown)
-root.bind('<Right>', lookRight)
-root.bind('<Left>', lookLeft)
-root.bind(3, speedHigh)
-root.bind(2, speedMed)
-root.bind(1, speedLow)
+    def speedLow(e):
+        myLabel = Label(self, text="Speed 1")
+        myLabel.pack()
+
+    def stop(e):
+        myLabel = Label(self, text="Stop")
+        myLabel.pack()
+
+    self.bind('<w>', forward)
+    self.bind('<s>', backward)
+    self.bind('<d>', turnRight)
+    self.bind('<a>', turnLeft)
+    self.bind('<e>', twistRight)
+    self.bind('<q>', twistLeft)
+    self.bind('<Up>', lookUp)
+    self.bind('<Down>', lookDown)
+    self.bind('<Right>', lookRight)
+    self.bind('<Left>', lookLeft)
+    self.bind(3, speedHigh)
+    self.bind(2, speedMed)
+    self.bind(1, speedLow)
+    self.bind('<z>', stop)
 
 
-root.mainloop()
+    self.mainloop()
 
 
 #------------------
