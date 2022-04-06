a6be7d1e5e93f9e647311e394ce169e4d27cb96a
ronnelwalton <59928379+ronnelwalton@users.noreply.github.com>
2022-04-06 12:26:00 -0600

    Update conversation.py

--- TangoBot/Voice and Reconition/conversation.py
+++ TangoBot/Voice and Reconition/conversation.py
@@ -2,42 +2,48 @@ import _thread, threading
 from tokenize import String
 import speech_recognition as sr
 import time
+import pyttsx3
 
 class Conversation:
 
     def __init__(self):
         self.currentPhrase = ""
         self.ready = False
+        self.engine = pyttsx3.init()
+        self.voice_num = 2
+        voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice', voices[self.voice_num].id)
+        self.speakTh = True
+        self.listenTh = True
         return
 
     def speak_thread(self):
         # Speak the currentPhrase when ready is set to true
-
-        if self.ready:
-            #speak
-            print(self.currentPhrase)
-        return
+        while(self.speakTh):
+            if self.ready:
+                #speak
+                self.engine.say(self.currentPhrase)
+                self.engine.runAndWait()
+                print(self.currentPhrase)
+                self.currentPhrase = ""
+                self.ready = False
 
     def listen_thread(self):
         # Listen to the user (Main Thread)
-        while(True):
+        while(self.listenTh):
             r = sr.Recognizer()
             speech = sr.Microphone(device_index=6)
             with speech as source:
                 audio = r.listen(source, phrase_time_limit=3)
             try:
                 recog = r.recognize_google(audio, language = 'en-US')
-
-
-
+                # Test
+                self.currentPhrase = recog
+                self.ready = True
             except sr.UnknownValueError:
                 print("Google Speech Recognition could not understand audio")
             except sr.RequestError as e:
                 print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
-        return
-
     pass
 
 class Dialog_Engine:
@@ -81,3 +87,8 @@ class Dialog_Engine:
         return
     
     pass
+
+inst = Conversation()
+
+_thread.start_new_thread(inst.speak_thread,())
+inst.listen_thread()
