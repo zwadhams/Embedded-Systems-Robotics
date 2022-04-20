15459fca1da96c93722b6f1b0ff3498404341a85
Zach Wadhams <33103466+zwadhams@users.noreply.github.com>
2022-04-20 12:36:55 -0600

    Update GUI.py

--- TangoBot/GUI.py
+++ TangoBot/GUI.py
@@ -9,6 +9,53 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from TextToSpeech import *
 
+import speech_recognition as sr
+import time, serial, sys
+from TangoController import *
+usb = serial.Serial('/dev/ttyACM0')
+tangoController = Tango_Controller(usb)
+from tkinter import *
+from time import sleep
+
+self = Tk()
+
+
+def forward():
+        if (tangoController.motors['Right_Wheel'].get_speed() == 0):
+                tangoController.adjust_backward_forward(2)
+        else:
+                tangoController.adjust_backward_forward(1)
+
+def backward():
+        if (tangoController.motors['Right_Wheel'].get_speed() == 0):
+                tangoController.adjust_backward_forward(-2)
+        else:
+                tangoController.adjust_backward_forward(-1)
+
+def turnRight():
+        tangoController.adjust_left_right(3)
+
+def turnLeft():
+        tangoController.adjust_left_right(-3)
+
+def twistRight():
+        tangoController.adjust_pan_waist(-1)
+
+def twistLeft():
+        tangoController.adjust_pan_waist(1)
+
+def lookUp():
+        tangoController.adjust_tilt_neck(1)
+
+def lookDown():
+        tangoController.adjust_tilt_neck(-1)
+
+def lookRight():
+        tangoController.adjust_pan_neck(-1)
+
+def lookLeft():
+        tangoController.adjust_pan_neck(1)
+
 
 class MyApp(App):
 
@@ -48,13 +95,16 @@ class MyApp(App):
         pass
 
     def headTilt():
-        pass
+        lookUp()
+        lookDown()
 
     def headPan():
-        pass
+        lookRight()
+        lookLeft()
 
     def waistTurn():
-        pass
+        twistRight()
+        twistLeft()
 
     def speechInput():
         pass
