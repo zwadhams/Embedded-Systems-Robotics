d7da93e913f18d331eb6e69cd1116638f95d880d
Kruize Derek Christensen <christensen.kruize@gmail.com>
2022-04-20 22:40:53 -0600

    Add files via upload
    Added the custom buttons

--- TangoBot/GUI.py
+++ TangoBot/GUI.py
@@ -10,83 +10,226 @@ from kivy.uix.button import Button
 from kivy.core.window import Window
 from TextToSpeech import *
 
-import speech_recognition as sr
-import time, serial, sys
-from TangoController import *
-usb = serial.Serial('/dev/ttyACM0')
-tangoController = Tango_Controller(usb)
-from tkinter import *
-from time import sleep
-
-self = Tk()
 
 
-def forward():
-        if (tangoController.motors['Right_Wheel'].get_speed() == 0):
-                tangoController.adjust_backward_forward(2)
-        else:
-                tangoController.adjust_backward_forward(1)
+class MyGridLayout(GridLayout):
+    # Initialize infinite keywords
+    def __init__(self, **kwargs):
+        #Call grid layout constructor
+        super(MyGridLayout, self).__init__(**kwargs)
 
-def backward():
-        if (tangoController.motors['Right_Wheel'].get_speed() == 0):
-                tangoController.adjust_backward_forward(-2)
-        else:
-                tangoController.adjust_backward_forward(-1)
+        #Set colums
+        self.rows = 6
+        self.columns = 3
 
-def turnRight():
-        tangoController.adjust_left_right(3)
+        #add widgets
+        self.lookLeft = Button(color =(1, 0, .65, 1),
+                    background_normal = 'Buttons/normal/lookLeft.png',
+                    background_down ='Buttons/down/lookLeft.png',
+                    size_hint_y = None,
+                    height=100,
+                    size_hint_x = None,
+                    width=100,
+                   )
+        self.lookLeft.bind(on_press=self.pressLookLeft)
+        self.add_widget(self.lookLeft)
+        ####################################################
+        self.lookRight = Button(color =(1, 0, .65, 1),
+                    background_normal = 'Buttons/normal/lookRight.png',
+                    background_down ='Buttons/down/lookRight.png',
+                    size_hint_y = None,
+                    height=100,
+                    size_hint_x = None,
+                    width=100,
+                   )
+        self.lookRight.bind(on_press=self.pressLookRight)
+        self.add_widget(self.lookRight)
+        ####################################################
+        self.lookDown = Button(color =(1, 0, .65, 1),
+                    background_normal = 'Buttons/normal/lookDown.png',
+                    background_down ='Buttons/down/lookDown.png',
+                    size_hint_y = None,
+                    height=100,
+                    size_hint_x = None,
+                    width=100,
+                   )
+        self.lookDown.bind(on_press=self.pressLookDown)
+        self.add_widget(self.lookDown)
+        ####################################################
+        self.lookUp = Button(color =(1, 0, .65, 1),
+                    background_normal = 'Buttons/normal/lookUp.png',
+                    background_down ='Buttons/down/lookUp.png',
+                    size_hint_y = None,
+                    height=100,
+                    size_hint_x = None,
+                    width=100,
+                   )
+        self.lookUp.bind(on_press=self.pressLookUp)
+        self.add_widget(self.lookUp)
+        ####################################################
+        self.twistLeft = Button(color =(1, 0, .65, 1),
+                    background_normal = 'Buttons/normal/twistLeft.png',
+                    background_down ='Buttons/down/twistLeft.png',
+                    size_hint_y = None,
+                    height=100,
+                    size_hint_x = None,
+                    width=100,
+                   )
+        self.twistLeft.bind(on_press=self.pressTwistLeft)
+        self.add_widget(self.twistLeft)
+        ####################################################
+        self.twistRight = Button(color =(1, 0, .65, 1),
+                    background_normal = 'Buttons/normal/twistRight.png',
+                    background_down ='Buttons/down/twistRight.png',
+                    size_hint_y = None,
+                    height=100,
+                    size_hint_x = None,
+                    width=100,
+                   )
+        self.twistRight.bind(on_press=self.pressTwistRight)
+        self.add_widget(self.twistRight)
+        ####################################################
+        self.turnLeft = Button(color =(1, 0, .65, 1),
+                    background_normal = 'Buttons/normal/turnLeft.png',
+                    background_down ='Buttons/down/turnLeft.png',
+                    size_hint_y = None,
+                    height=100,
+                    size_hint_x = None,
+                    width=100,
+                   )
+        self.turnLeft.bind(on_press=self.pressTurnLeft)
+        self.add_widget(self.turnLeft)
+        ####################################################
+        self.turnRight = Button(color =(1, 0, .65, 1),
+                    background_normal = 'Buttons/normal/turnRight.png',
+                    background_down ='Buttons/down/turnRight.png',
+                    size_hint_y = None,
+                    height=100,
+                    size_hint_x = None,
+                    width=100,
+                   )
+        self.turnRight.bind(on_press=self.pressTurnRight)
+        self.add_widget(self.turnRight)
+        ####################################################
+        self.Forewards = Button(color =(1, 0, .65, 1),
+                    background_normal = 'Buttons/normal/Forewards.png',
+                    background_down ='Buttons/down/Forewards.png',
+                    size_hint_y = None,
+                    height=100,
+                    size_hint_x = None,
+                    width=100,
+                   )
+        self.Forewards.bind(on_press=self.pressForewards)
+        self.add_widget(self.Forewards)
+        ####################################################
+        self.Backwards = Button(color =(1, 0, .65, 1),
+                    background_normal = 'Buttons/normal/Backwards.png',
+                    background_down ='Buttons/down/Backwards.png',
+                    size_hint_y = None,
+                    height=100,
+                    size_hint_x = None,
+                    width=100,
+                   )
+        self.Backwards.bind(on_press=self.pressBackwards)
+        self.add_widget(self.Backwards)
+        ####################################################
+        self.RUN = Button(color =(1, 0, .65, 1),
+                    background_normal = 'Buttons/normal/RUN.png',
+                    background_down ='Buttons/down/RUN.png',
+                    size_hint_y = None,
+                    height=100,
+                    size_hint_x = None,
+                    width=100,
+                   )
+        self.RUN.bind(on_press=self.pressRUN)
+        self.add_widget(self.RUN)
+        ####################################################
+        self.reset = Button(color =(1, 0, .65, 1),
+                    background_normal = 'Buttons/normal/reset.png',
+                    background_down ='Buttons/down/reset.png',
+                    size_hint_y = None,
+                    height=100,
+                    size_hint_x = None,
+                    width=100,
+                   )
+        self.reset.bind(on_press=self.pressReset)
+        self.add_widget(self.reset)
+        ####################################################
 
-def turnLeft():
-        tangoController.adjust_left_right(-3)
+    def pressRUN(self,instance):
+        talkBack("RUN")
+        
+    def pressReset(self,instance):
+        talkBack("reset")
 
-def twistRight():
-        tangoController.adjust_pan_waist(-1)
+    def pressBackwards(self,instance):
+        talkBack("Backwards")
+        
+    def pressForewards(self,instance):
+        talkBack("Forewards")
 
-def twistLeft():
-        tangoController.adjust_pan_waist(1)
+    def pressTurnRight(self,instance):
+        talkBack("Turn Right")
+        
+    def pressTurnLeft(self,instance):
+        talkBack("Turn Left")
 
-def lookUp():
-        tangoController.adjust_tilt_neck(1)
+    def pressTwistRight(self,instance):
+        talkBack("Twist Right")
+        
+    def pressTwistLeft(self,instance):
+        talkBack("Twist Left")
 
-def lookDown():
-        tangoController.adjust_tilt_neck(-1)
+    def pressLookUp(self,instance):
+        talkBack("Look Up")
+        
+    def pressLookDown(self,instance):
+        talkBack("Look Down")
 
-def lookRight():
-        tangoController.adjust_pan_neck(-1)
-
-def lookLeft():
-        tangoController.adjust_pan_neck(1)
-
+    def pressLookRight(self,instance):
+        talkBack("Look Right")
+        
+    def pressLookLeft(self,instance):
+        talkBack("Look Left")
+        
 
 class MyApp(App):
+    
 
     def build(self):
         Window.fullscreen = True
-        self.window = GridLayout()
-        self.icon = 'hello.jpg'
-        self.window.cols = 1
-        self.window.size_hint = (0.6, 0.7)
-        self.window.pos_hint = {"center_x" : 0.5, "center_y" : 0.5}
-        self.window.add_widget(Image(source='hello.jpg',
-                                     size_hint = (3,3)))
-        self.question = Label(text="What is your robot's name?",
-                              font_size = 36,
-                              color = '#00abFF')
-        self.window.add_widget(self.question)
-        self.answer = TextInput(multiline = False,
-                                padding_y = (20 ,20),
-                                padding_x = (10,10),
-                                size_hint = (1, 0.9))
-        self.window.add_widget(self.answer)
-        self.button = Button(text="Talk")
-        self.button.bind(on_press = self.callback)
-        self.window.add_widget(self.button)
+        return MyGridLayout()
+##        self.window = GridLayout()
+##        self.icon = 'hello.jpg'
+##        self.window.cols = 1
+##        self.window.pos_hint = {"center_x" : 0.5, "center_y" : 0.5}
+##        self.window.add_widget(Image(source='hello.jpg',
+##                                     size_hint = (3,3)))
+##        self.question = Label(text="What is your robot's name?",
+##                              font_size = 36,
+##                              color = '#00abFF')
+##        self.window.add_widget(self.question)
+##        self.answer = TextInput(multiline = False,
+##                                padding_y = (20 ,20),
+##                                padding_x = (10,10),
+##                                size_hint = (1, 0.9))
+##        self.window.add_widget(self.answer)
+
+##        self.button1 = Button(color =(1, 0, .65, 1),
+##                     background_normal = 'Buttons/normal/RUN.png',
+##                     background_down ='Buttons/down/RUN.png',
+##                     size_hint = (.3, .3),
+##                     pos_hint = {"x":0.35, "y":0.3}
+##                   )
+##        self.button1.bind(on_press = self.callback)
+##        self.window.add_widget(self.button1)
         
         return self.window
 
     def callback(self, instance):
         print(instance)
         self.question.text = "Hello " + self.answer.text + "!"
+        talkBack(self.question.text)
  
 
 ########These are the 7 actions our robot has to do########
@@ -97,42 +240,18 @@ class MyApp(App):
         pass
 
     def headTilt():
-        lookUp()
-        lookDown()
+        pass
 
     def headPan():
-        lookRight()
-        lookLeft()
+        pass
 
     def waistTurn():
-        twistRight()
-        twistLeft()
+        pass
 
     def speechInput():
-        talkBack("Say something please")
-        flag = True
-        while(flag):
-            r = sr.Recognizer()
-            speech = sr.Microphone(device_index=0)
-            with speech as source:
-                audio = r.adjust_for_ambient_noise(source)
-                audio = r.listen(source)
-            try:
-                recog = r.recognize_google(audio, language = 'en-US')
-                if recog:
-                    talkBack("Okay I heard you but I really dont care")
-                    flag = False
-                else:
-                    talkBack("Yeah you didnt say anything, whatever")
-                    flag = False
-            except sr.UnknownValueError:
-               talkBack("bbebrbbbrbbrbe, dead")
+        pass
 
-    def speechOutput(userInput):
-        talkBack(userInput)
-        
-
-    speechOutput("nope")
+    
 
 def main():
     MyApp().run()
