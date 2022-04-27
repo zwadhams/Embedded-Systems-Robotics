5157d20f92aea19d97f22cc2373b9c25a7547fa0
Zach Wadhams <33103466+zwadhams@users.noreply.github.com>
2022-04-27 11:55:50 -0600

    Add files via upload

---
+++ TangoBot/Final/movement.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+gameMap1 = np.array([[0, 1, 0],
+                    [2, 3, 4],
+                    [0, 5, 0]])
+
+gameMap2 = np.array([[1, 2, 3],
+                    ['x', 'o', 'x'],
+                    [6, 7, 8],
+                    ['o', 'o', 'x']
+                    [11, 12, 13]])
+
+print(gameMap1)
+print(gameMap2)    
