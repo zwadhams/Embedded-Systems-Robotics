22c3846cf3df91a5130abca3a5eebcbd3bb02153
Zach Wadhams <33103466+zwadhams@users.noreply.github.com>
2022-04-27 13:02:22 -0600

    updated with node connections

--- TangoBot/Final/movement.py
+++ TangoBot/Final/movement.py
@@ -1,15 +1,26 @@
-import numpy as np
+
+class Node:
+    def __init__(self, key):
+        self.id = key
+        self.connected_to = {}
 
 
-gameMap1 = np.array([[0, 1, 0],
-                    [2, 3, 4],
-                    [0, 5, 0]])
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n6 = Node(6)
+n7 = Node(7)
+n8 = Node(8)
+n11 = Node(11)
+n12 = Node(12)
+n13 = Node(13)
 
-gameMap2 = np.array([[1, 2, 3],
-                    ['x', 'o', 'x'],
-                    [6, 7, 8],
-                    ['o', 'o', 'x']
-                    [11, 12, 13]])
-
-print(gameMap1)
-print(gameMap2)    
+n1.connected_to = {n2}
+n2.connected_to = {n1, n3, n7}
+n3.connectged_to = {n2, n8}
+n6.connected_to = {n11, n7}
+n7.connected_to = {n2, n6, n12}
+n8.connected_to = {n3}
+n11.connected_to = {n6}
+n12.connected_to = {n7, n13}
+n13.connected_to = {n12}
