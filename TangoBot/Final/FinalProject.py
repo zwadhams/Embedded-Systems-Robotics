544aa6a48c04e69e524600a72e23df99778dddaa
ronnelwalton <59928379+ronnelwalton@users.noreply.github.com>
2022-04-27 13:08:02 -0600

    Update FinalProject.py

--- TangoBot/Final/FinalProject.py
+++ TangoBot/Final/FinalProject.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import time, serial, sys
+from tkinter import *
+from TangoController import *
+
+def main():
+
+    usb = serial.Serial('/dev/ttyACM0')
+    tangoController = Tango_Controller(usb)
+
+    return
