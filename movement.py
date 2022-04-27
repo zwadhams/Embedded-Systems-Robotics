3cdf1704cbdcc6ed80d2833313f823acb2d5bea5
Zach Wadhams <33103466+zwadhams@users.noreply.github.com>
2022-04-27 13:32:49 -0600

    Add files via upload

---
+++ movement.py
@@ -0,0 +1,45 @@
+
+class Node:
+    def __init__(self, key):
+        self.id = key
+        self.connected_to = {}
+        self.visited = False
+        self.previous = None
+
+
+    def get_id(self):
+        return self.id
+
+    def get_connections(self):
+        return self.connected_to.keys()
+
+    def set_visited(self):
+        self.visited = True
+
+    def set_previous(self):
+        self.previous = prev
+
+
+#creates nodes
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n6 = Node(6)
+n7 = Node(7)
+n8 = Node(8)
+n11 = Node(11)
+n12 = Node(12)
+n13 = Node(13)
+
+#creates node connections
+n1.connected_to = {n2}
+n2.connected_to = {n1, n3, n7}
+n3.connectged_to = {n2, n8}
+n6.connected_to = {n11, n7}
+n7.connected_to = {n2, n6, n12}
+n8.connected_to = {n3}
+n11.connected_to = {n6}
+n12.connected_to = {n7, n13}
+n13.connected_to = {n12}
+
+
